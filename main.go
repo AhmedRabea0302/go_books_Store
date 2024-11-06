d7c31044e007549d310e96be62ad72cab0d33ce7
AhmedRabea0302 <arabea169@gmail.com>
2024-11-06 15:23:55 +0200

    Added Routes

--- main.go
+++ main.go
@@ -8,20 +8,54 @@ import (
 	"gorm.io/gorm"
 )
 
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
 type Repository struct {
 	DB *gorm.DB
 }
 
 func (r Repository) SetupRputes(app *fiber.App) {
 	api := app.Group("/api")
-	api.Post("book/create", r.CreateBook)
-	api.Delete("book/delete", r.DeleteBook)
+
+	api.Get("book/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
+	api.Post("book/", r.CreateBook)
+	api.Delete("book/:id", r.DeleteBook)
+
 }
 
+// Get Book By ID
+func (r Repository) GetBookByID(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (r Repository) GetBooks(ctx *fiber.Ctx) error {
+	return nil
+
+}
+
+func (r Repository) CreateBook(ctx *fiber.Ctx) error {
+	return nil
+
+}
+
+func (r Repository) DeleteBook(ctx *fiber.Ctx) error {
+	return nil
+
+}
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("Couldn't connect to DB", err)
 	}
 
 	r := Repository{
